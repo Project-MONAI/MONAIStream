1a1bcbc8ae612b7596aee3c0a0e5b4d9414c3790
Alvin Ihsani <aihsani@nvidia.com>
2021-08-15 15:54:57 -0400

    updates to container startup to ensure egl video displays

--- start_devel.sh
+++ start_devel.sh
@@ -5,6 +5,8 @@ docker run --gpus all \
            --rm \
            -it \
            --shm-size=1g --ulimit memlock=-1 \
+           -v /tmp/.X11-unix:/tmp/.X11-unix \
+           -v ${HOME}/.Xauthority:/home/user/.Xauthority \
            -v ${PWD}:/app \
            -w /app \
            -e DISPLAY \
