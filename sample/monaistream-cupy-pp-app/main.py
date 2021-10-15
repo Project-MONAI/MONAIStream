42b684f51e3d707efc46f7face7266ba0cb4058b
Anisha Goel <anishag@nvidia.com>
2021-10-15 14:04:52 -0700

    Add app with CuPy post processing
    This app works for both x86 and
    Clara AGX Dev Kit with dGPU.

---
+++ sample/monaistream-cupy-pp-app/main.py
@@ -0,0 +1,62 @@
+import logging
+
+import cupy
+import cupy.cuda.cudnn
+import cupy.cudnn
+import numpy as np
+
+from typing import List
+
+from monaistream.compose import StreamCompose
+from monaistream.filters import FilterProperties, NVInferServer, NVStreamMux, NVVideoConvert
+from monaistream.filters.transform_cupy import TransformChainComponentCupy
+from monaistream.sinks import NVEglGlesSink
+from monaistream.sources import NVAggregatedSourcesBin, URISource
+
+logging.basicConfig(level=logging.DEBUG)
+
+def color_blender(img: cupy.ndarray, mask: List[cupy.ndarray]):
+    if mask:
+        # mask is of range [0,1]
+        mask[0] = cupy.cudnn.activation_forward(mask[0], cupy.cuda.cudnn.CUDNN_ACTIVATION_SIGMOID)
+        # modify only the red channel in-place to apply mask
+        img[..., 0] = cupy.multiply(1.0 - mask[0], img[..., 0])
+    return
+
+if __name__ == "__main__":
+
+    infer_server_config = NVInferServer.generate_default_config()
+    infer_server_config.infer_config.backend.trt_is.model_repo.root = "/media/m2/monai-stream-experimental/models"
+    infer_server_config.infer_config.backend.trt_is.model_name = "monai_unet_trt"
+    infer_server_config.infer_config.backend.trt_is.version = "-1"
+    infer_server_config.infer_config.backend.trt_is.model_repo.log_level = 0
+
+    chain = StreamCompose(
+        [
+            NVAggregatedSourcesBin(
+                [
+                    URISource(uri="file:///media/m2/monai-stream-experimental/videos/d1_im.mp4"),
+                ]
+            ),
+            NVStreamMux(
+                num_sources=1,
+                width=1260,
+                height=1024,
+            ),
+            NVVideoConvert(
+                FilterProperties(
+                    format="RGBA",
+                    width=1264,
+                    height=1024,
+                )
+            ),
+            NVInferServer(
+                config=infer_server_config,
+            ),
+            TransformChainComponentCupy(
+                transform_chain = color_blender
+            ),
+            NVEglGlesSink(sync=True),
+        ]
+    )
+    chain()
--- src/monaistream/filters/__init__.py
+++ src/monaistream/filters/__init__.py
@@ -2,3 +2,4 @@ from .convert import FilterProperties, NVVideoConvert
 from .infer import *
 from .nvstreammux import NVStreamMux
 from .transform import TransformChainComponent
+from .transform_cupy import TransformChainComponentCupy
